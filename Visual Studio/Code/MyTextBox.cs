de85a680d019c0d9dbbe93f95d697b246cf3ab04
Scott <scottmwyant@gmail.com>
2017-09-03 02:20:43 -0400

    Requesting pull to master. (#1)
    * Establish relationships between project files
    
    When opening the .sln file, the project file was not found in the expected location.  It seems that files were added using the "folder view" of the solution explorer instead of the solution view, which would presumably create the files without linking them to the solution.
    
    * BuildingSolution
    
    Code compiles.  The "AAS" case has been validated.  Scope of work is far greater than the name of the branch implies.  This branch needs to be pulled to the master.  From there, it should be taken back down into a generic "Build" branch.

--- Visual Studio/Code/MyTextBox.cs
+++ Visual Studio/Code/MyTextBox.cs
@@ -1,28 +1,31 @@
-﻿using System;
+﻿// References
+using System;
+using System.Windows.Forms;
 
 namespace TrigSolver
 {
     public class MyTextBox : TextBox
     {
-        // Constructor
-        public MyTextBox()
-        {
-
-        }
-
+        // Fields
+        private double myValue = new double();
+        
         // Properties
+        public double Value { get { return myValue; } set { myValue = value; } }
         public string FeatureType { get; set; }
         public bool Valid { get; set; }
         public bool Input { get; set; }
 
-        public double NumericValue
+        // Methods
+        public void UpdateNumericValue()
         {
-            get
-            {
-                double val = new double();
-                double.TryParse(this.Text, out val);
-                return val;
-            }
+            double temp = new double();
+            double.TryParse(this.Text, out temp);
+            myValue = temp;
+        }
+
+        public void UpdateText(byte precision)
+        {
+            this.Text = Convert.ToString(Math.Round(this.Value, precision));
         }
     }
 }
