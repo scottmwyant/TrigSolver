10ecc994232352c47c89f3163292a780717eb493
Scott Wyant <scottmwyant@gmail.com>
2018-08-01 22:01:46 -0400

    Update VS config file.
    app.config is in the properties sub-folder

---
+++ dto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace AppCore
+{
+
+    public class Corner
+    {
+        private double angle = new double();
+        private bool useDegrees = new bool();
+
+        public double Length { get; set; }
+
+        public bool UseDegrees { get; set; }
+        public double Angle
+        {
+            get
+            {
+                if (useDegrees) { return angle * 180 / Math.PI; }
+                else { return angle; }
+            }
+            set
+            {
+                if (useDegrees) { angle = value * Math.PI / 180; }
+                else { angle = value; }
+            }
+        }
+    }
+
+}
