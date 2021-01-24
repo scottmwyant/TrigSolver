e8ad9d3a3a2c690cb1753733f04cff789da40e66
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2021-01-24 07:26:45 -0500

    Move data from deadcode.ts to utilities.ts

--- src/utiliies.ts
+++ src/utiliies.ts
@@ -102,6 +102,37 @@ export function profile(inputs: DataPoint[]) {
       { name: "pair-and-side", scope: [25, 26, 41, 44, 50, 52] },
       { name: "pair-and-angle", scope: [11, 13, 19, 22, 37, 38] }
     ]
+    
+    /*
+    
+      This array is included here in the source code and intentionally
+      not used.  This is the only place we see the binaryId alongside
+      the decimal form of the id and the algorithm name.
+    */
+   
+    // const referenceData = [
+    //   { "binary": "111000", "id": 56, "algorithm": "side-side-side" },
+    //   { "binary": "001110", "id": 14, "algorithm": "side-angle-angle" },
+    //   { "binary": "010101", "id": 21, "algorithm": "side-angle-angle" },
+    //   { "binary": "100011", "id": 35, "algorithm": "side-angle-angle" },
+    //   { "binary": "110001", "id": 49, "algorithm": "side-side-angle" },
+    //   { "binary": "011100", "id": 28, "algorithm": "side-side-angle" },
+    //   { "binary": "101010", "id": 42, "algorithm": "side-side-angle" },
+    //   { "binary": "110100", "id": 52, "algorithm": "pair-and-side" },
+    //   { "binary": "101100", "id": 44, "algorithm": "pair-and-side" },
+    //   { "binary": "110010", "id": 50, "algorithm": "pair-and-side" },
+    //   { "binary": "011010", "id": 26, "algorithm": "pair-and-side" },
+    //   { "binary": "101001", "id": 41, "algorithm": "pair-and-side" },
+    //   { "binary": "011001", "id": 25, "algorithm": "pair-and-side" },
+    //   { "binary": "100110", "id": 38, "algorithm": "pair-and-angle" },
+    //   { "binary": "100101", "id": 37, "algorithm": "pair-and-angle" },
+    //   { "binary": "010110", "id": 22, "algorithm": "pair-and-angle" },
+    //   { "binary": "010011", "id": 19, "algorithm": "pair-and-angle" },
+    //   { "binary": "001101", "id": 13, "algorithm": "pair-and-angle" },
+    //   { "binary": "001011", "id": 11, "algorithm": "pair-and-angle" },
+    //   { "binary": "000111", "id": 7, "algorithm": "no-solution" }
+    // ];
+
     const arr = data.filter(item => (item.scope.indexOf(caseId) > -1));
     return arr.length == 1 ? arr[0].name : 'no-solution';
   })();
